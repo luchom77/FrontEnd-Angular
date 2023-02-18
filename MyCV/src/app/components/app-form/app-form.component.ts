a57ec1e03874e0dbee7c010019f709e3fba7afee
Luchom7 <luchomooreventi@yahoo.com>
2023-02-18 01:42:03 -0300

    10

--- MyCV/src/app/components/app-form/app-form.component.ts
+++ MyCV/src/app/components/app-form/app-form.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-app-form',
   templateUrl: './app-form.component.html',
-  styleUrls: ['./app-form.component.css']
+  styleUrls: ['./app-form.component.css'],
 })
-export class AppFormComponent {
-
-}
+export class AppFormComponent {}
