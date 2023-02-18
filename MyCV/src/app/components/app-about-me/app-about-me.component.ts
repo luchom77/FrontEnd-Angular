5a64e5e4b135cac8040727bd8c69fdacb205b13f
Luchom7 <luchomooreventi@yahoo.com>
2023-02-18 17:33:49 -0300

    12

--- MyCV/src/app/components/app-about-me/app-about-me.component.ts
+++ MyCV/src/app/components/app-about-me/app-about-me.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-app-about-me',
+  selector: 'app-about-me',
   templateUrl: './app-about-me.component.html',
-  styleUrls: ['./app-about-me.component.css']
+  styleUrls: ['./app-about-me.component.css'],
 })
-export class AppAboutMeComponent {
+export class AppAboutMeComponent implements OnInit {
+  nombre = 'Luciano Moore';
+  profession = 'Designer and Web Developer';
 
+  getProfession() {
+    return this.profession;
+  }
+
+  constructor() {}
+  ngOnInit(): void {}
 }
